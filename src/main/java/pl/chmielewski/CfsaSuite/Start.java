7564ec57bdac9655ef1dfb9d73884e3a963e4092
Blazej Chmielewski <blazejchmielewski01.gmail.com>
2023-07-31 11:56:29 +0200

    Tworze klase testowa gdzie na sztywko wrzucam uzytkownika

--- src/main/java/pl/chmielewski/CfsaSuite/Start.java
+++ src/main/java/pl/chmielewski/CfsaSuite/Start.java
@@ -1,4 +1,25 @@
 package pl.chmielewski.CfsaSuite;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+import pl.chmielewski.CfsaSuite.entity.CfsaUser;
+import pl.chmielewski.CfsaSuite.repository.CfsaUserRepo;
+
+@Component
 public class Start {
+
+    private BCryptPasswordEncoder passwordEncoder;
+    private CfsaUserRepo cfsaUserRepo;
+
+    @Autowired
+    public Start(BCryptPasswordEncoder passwordEncoder, CfsaUserRepo cfsaUserRepo) {
+        this.passwordEncoder = passwordEncoder;
+        this.cfsaUserRepo = cfsaUserRepo;
+
+        CfsaUser cfsaUser = new CfsaUser();
+        cfsaUser.setUsername("Przemek");
+        cfsaUser.setPassword(passwordEncoder.encode("Przemek123"));
+        cfsaUserRepo.save(cfsaUser);
+    }
 }
