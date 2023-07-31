83bd2741eff81fd64c8c6d209015b80368474222
Blazej Chmielewski <blazejchmielewski01.gmail.com>
2023-07-31 12:49:10 +0200

    Dodaje wstrzykniecie password encoder

--- src/main/java/pl/chmielewski/CfsaSuite/Start.java
+++ src/main/java/pl/chmielewski/CfsaSuite/Start.java
@@ -1,7 +1,7 @@
 package pl.chmielewski.CfsaSuite;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import pl.chmielewski.CfsaSuite.entity.CfsaUser;
 import pl.chmielewski.CfsaSuite.repository.CfsaUserRepo;
@@ -9,11 +9,11 @@ import pl.chmielewski.CfsaSuite.repository.CfsaUserRepo;
 @Component
 public class Start {
 
-    private BCryptPasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
     private CfsaUserRepo cfsaUserRepo;
 
     @Autowired
-    public Start(BCryptPasswordEncoder passwordEncoder, CfsaUserRepo cfsaUserRepo) {
+    public Start(PasswordEncoder passwordEncoder, CfsaUserRepo cfsaUserRepo) {
         this.passwordEncoder = passwordEncoder;
         this.cfsaUserRepo = cfsaUserRepo;
 
