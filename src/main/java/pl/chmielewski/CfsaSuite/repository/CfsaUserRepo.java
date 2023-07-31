eabe4e951c01feb1f7ed4345ac8b85cb74c1e996
Blazej Chmielewski <blazejchmielewski01.gmail.com>
2023-07-31 11:56:05 +0200

    Tworze repo dla encji urzytkownika

--- src/main/java/pl/chmielewski/CfsaSuite/repository/CfsaUserRepo.java
+++ src/main/java/pl/chmielewski/CfsaSuite/repository/CfsaUserRepo.java
@@ -1,8 +1,10 @@
 package pl.chmielewski.CfsaSuite.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.chmielewski.CfsaSuite.entity.CfsaUser;
 
+@Repository
 public interface CfsaUserRepo extends JpaRepository<CfsaUser, Long> {
 
 }
