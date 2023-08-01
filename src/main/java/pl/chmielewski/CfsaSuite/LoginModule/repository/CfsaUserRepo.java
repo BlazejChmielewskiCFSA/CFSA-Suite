596e657faa1b2c157ba05759709300c5a28f37da
Blazej Chmielewski <blazejchmielewski01.gmail.com>
2023-08-01 10:56:55 +0200

    Korekcja CfsaUserRepo -> CfsaUser (czeski blad)

--- src/main/java/pl/chmielewski/CfsaSuite/LoginModule/repository/CfsaUserRepo.java
+++ src/main/java/pl/chmielewski/CfsaSuite/LoginModule/repository/CfsaUserRepo.java
@@ -1,8 +1,8 @@
-package pl.chmielewski.CfsaSuite.repository;
+package pl.chmielewski.CfsaSuite.LoginModule.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pl.chmielewski.CfsaSuite.entity.CfsaUser;
+import pl.chmielewski.CfsaSuite.LoginModule.entity.CfsaUser;
 
 @Repository
 public interface CfsaUserRepo extends JpaRepository<CfsaUser, Long> {
