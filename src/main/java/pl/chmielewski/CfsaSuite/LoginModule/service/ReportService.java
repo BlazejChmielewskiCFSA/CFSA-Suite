84f2d973e4dd7244bcb277ab0f2eeec57866da9b
blazej.chmielewski <blazej.chmielewski@cfsa.pl>
2023-08-11 13:36:03 +0200

    rozbudowywuje metode addNewReport zeby ustawiała status

--- src/main/java/pl/chmielewski/CfsaSuite/LoginModule/service/ReportService.java
+++ src/main/java/pl/chmielewski/CfsaSuite/LoginModule/service/ReportService.java
@@ -2,11 +2,17 @@ package pl.chmielewski.CfsaSuite.LoginModule.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.chmielewski.CfsaSuite.LoginModule.entity.CfsaUser;
 import pl.chmielewski.CfsaSuite.LoginModule.entity.Report;
+import pl.chmielewski.CfsaSuite.LoginModule.entity.enums.Status;
+import pl.chmielewski.CfsaSuite.LoginModule.repository.CfsaUserRepo;
 import pl.chmielewski.CfsaSuite.LoginModule.repository.ReportRepo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Service
 public class ReportService {
@@ -21,17 +27,20 @@ public class ReportService {
         this.userService = userService;
     }
 
-    public List<Report> getAllReports(){
+    public List<Report> getAllReports() {
         return reportRepo.findAll();
     }
 
-    public void addNewReport(Report newReport){
-        newReport.setOwner(userService.getUserName());
-        reportList.add(newReport);
-        reportRepo.save(newReport);
+    public Report getReportById(Long id){
+        return reportRepo.findAllById(id);
     }
 
-    public String getReportOwner(){
-        return userService.getUserName();
+    public void addNewReport(Report newReport) {
+        newReport.setOwner(userService.getUserName());
+        newReport.setStatus(Status.Oczekujacy);
+        String header = newReport.getHeader().substring(0, 1).toUpperCase() + newReport.getHeader().substring(1);
+        newReport.setHeader(header);
+        reportList.add(newReport);
+        reportRepo.save(newReport);
     }
 }
