d27842bcc769300efdb8232b20da55c6c2e06131
Blazej Chmielewski <blazejchmielewski01.gmail.com>
2023-08-11 08:01:50 +0200

    Twroze repo dla reportu

---
+++ src/main/java/pl/chmielewski/CfsaSuite/LoginModule/repository/ReportRepo.java
@@ -0,0 +1,9 @@
+package pl.chmielewski.CfsaSuite.LoginModule.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.chmielewski.CfsaSuite.LoginModule.entity.Report;
+
+@Repository
+public interface ReportRepo extends JpaRepository<Report, Long> {
+}
