50a350ce4da5c8cb8fb35a37f2cf7ec557d5c2d8
blazej.chmielewski <blazej.chmielewski@cfsa.pl>
2023-08-23 14:52:37 +0200

    Dodaje obsługe formularza

--- src/main/java/pl/chmielewski/CfsaSuite/LoginModule/controller/MainController.java
+++ src/main/java/pl/chmielewski/CfsaSuite/LoginModule/controller/MainController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import pl.chmielewski.CfsaSuite.LoginModule.entity.CfsaUser;
 import pl.chmielewski.CfsaSuite.LoginModule.entity.Report;
 import pl.chmielewski.CfsaSuite.LoginModule.entity.enums.Departments;
+import pl.chmielewski.CfsaSuite.LoginModule.entity.enums.Priority;
+import pl.chmielewski.CfsaSuite.LoginModule.entity.enums.Status;
 import pl.chmielewski.CfsaSuite.LoginModule.service.ReportService;
 import pl.chmielewski.CfsaSuite.LoginModule.service.UserService;
 
@@ -48,7 +50,7 @@ public class MainController {
     public String signup(Model model) {
         CfsaUser cfsaUser = new CfsaUser();
         model.addAttribute("departamentsList", Departments.values());
-        model.addAttribute("newCfsaUser",cfsaUser);
+        model.addAttribute("newCfsaUser", cfsaUser);
         return "registration";
     }
 
@@ -64,6 +66,17 @@ public class MainController {
         return "redirect:/login";
     }
 
+    //Filtr
+    @PostMapping("/addFilter")
+    public String sendFilterPost(@RequestParam(name="statusValue", required = false, defaultValue = "false") Boolean statusValue,
+                                 @RequestParam(name="priorityValue", required = false, defaultValue = "false") Boolean priorityValue,
+                                 @RequestParam(name="departmentValue", required = false, defaultValue = "false") Boolean departmentValue,
+                                 @RequestParam(name="status", required = false) Status status,
+                                 @RequestParam(name="priority", required = false) Priority priority,
+                                 @RequestParam(name="departments", required = false) Departments departments) {
+        reportService.filtredList(statusValue, priorityValue, departmentValue, status, priority, departments);
+        return "redirect:/reports";
+    }
 
     //TWORZENIE NOWEGO ZGŁOSZENIA
     @GetMapping("/report-form")
@@ -84,12 +97,14 @@ public class MainController {
         List<Report> ReportsListMadeByLoggedUser = reportService.getAllReports().stream()
                 .filter(n -> n.getOwner().equals(userService.getLoggedUserHisUsername()))
                 .collect(Collectors.toList());
+        model.addAttribute("departments", Departments.values());
         model.addAttribute("ReportsListMadeByLoggedUser", ReportsListMadeByLoggedUser);
+        model.addAttribute("filtredList", reportService.getFiltredList());
         return "reports";
     }
 
     @GetMapping("/report/{id}")
-    public String reportDetails(@PathVariable Long id, Model model){
+    public String reportDetails(@PathVariable Long id, Model model) {
         Report report = reportService.getReportById(id);
         model.addAttribute("report", report);
         return "report-details";
